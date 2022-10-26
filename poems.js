119746c9e1455453c9ed18f1c3e6a6cb252e60e6
Cooper Midroni <34783043+Midroni@users.noreply.github.com>
2022-10-26 08:52:24 -0700

    Update poems.js

--- poems.js
+++ poems.js
@@ -2,10 +2,21 @@ const poems = [
 	{"title":`the internet is killing us`,
 "author":`cooper midroni`,
 "content":`
-- The internet
- - all this information
-- is killing us.
- - will destroy us.`
+-The world is more divided than ever. And it takes many forms.
+-A family at the dinner table 
+ -Friends
+-with
+-a chasm of misunderstanding
+ - a world of difference
+  - inescapable silence
+-between them.
+-The internet
+ -Information abundance
+-threatens our intellectual freedom, making us
+-apathetic
+ -afraid
+  -uncaring
+- with who to trust or what to read.`
 },
 	{"title":`the world is a sacred object`,
 "author":`kelsey chen`,
